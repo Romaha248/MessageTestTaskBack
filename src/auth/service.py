e0d772b18ffa6bd5e4453e2aa881b73ec52e57d0
Roman <romanbezverkhyi5@gmail.com>
2025-10-08 14:32:32 +0300

    change jwt error import

--- src/auth/service.py
+++ src/auth/service.py
@@ -9,7 +9,7 @@ from starlette import status
 from datetime import datetime, timezone, timedelta
 from sqlalchemy.orm import Session
 import jwt
-from jwt.exceptions import InvalidTokenError
+from jwt.exceptions import PyJWTError
 from uuid import UUID
 from typing import Annotated
 from fastapi import Depends, HTTPException, Response
@@ -76,7 +76,7 @@ def verify_token(token: str) -> TokenData:
                 status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token payload"
             )
         return TokenData(user_id=user_id, username=username)
-    except InvalidTokenError as e:
+    except PyJWTError as e:
         logging.warning(f"Token verification failed: {e}")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired token"
